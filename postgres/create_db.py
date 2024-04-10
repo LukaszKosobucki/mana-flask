e23bc9a726977ae82e5729908fe4b1555e81e53a
Lukasz Kosobucki <lukasz.kos98@gmail.com>
2024-04-10 21:03:51 +0200

    added db in compose, connected to db through script and populated

--- postgres/create_db.py
+++ postgres/create_db.py
@@ -1,9 +1,36 @@
-from sqlalchemy import create_engine,text
-import sqlalchemy
-
-engine = create_engine("postgresql+psycopg2://test:1234@localhost:5432/test",echo=True)
-engine.connect()
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
 
-print("works")
+def create_table():
+    engine = create_engine(
+        "postgresql+psycopg2://postgres:db-123456@db:5432/test", echo=True)
+    meta = MetaData()
 
+    students = Table(
+        'students', meta,
+        Column('id', Integer, primary_key=True),
+        Column('name', String),
+        Column('lastname', String),
+    )
+
+    meta.create_all(engine)
+
+    # ins = students.insert().values(name='daniel', lastname='kowalski')
+
+    connection = engine.connect()
+    # result = connection.execute(ins)
+
+    result = connection.execute(students.insert(), [
+        {'name': 'Rajiv', 'lastname': 'Khanna'},
+        {'name': 'Komal', 'lastname': 'Bhandari'},
+        {'name': 'Abdul', 'lastname': 'Sattar'},
+        {'name': 'Priya', 'lastname': 'Rajhans'},
+    ])
+
+    print(result.rowcount)
+    connection.commit()
+    connection.close()
+    print("works")
+
+
+create_table()
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flaskr.app import create_app
-
+from postgres.create_db import create_table
 
 if __name__ == "__main__":
+    create_table()
     create_app().run(host='0.0.0.0', port=8000, debug=True)
