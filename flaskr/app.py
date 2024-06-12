7663a97ad4ecbf1c5e089b1919904730d2dd9c18
Lukasz Kosobucki <lukasz.kosobucki@yougov.com>
2024-06-12 17:13:06 +0200

    added posts and deletes

--- flaskr/app.py
+++ flaskr/app.py
@@ -68,11 +68,45 @@ def create_app(test_config=None):
         except Exception as e:
             return {"error": str(e)}
         
+    @app.delete("/api/v1/students/<int:id>")
+    def delete_student(id):
+        try:
+            db.session.query(Student).filter(Student.id == id).delete()
+            db.session.commit()
+            return {"status_code": 200, "description": f"successfully deleted student" }
+        except Exception as e:
+            return {"error": str(e)}
+        
+    @app.post("/api/v1/students")
+    def post_student():
+        try: 
+            json = request.get_json()
+            student_name = json.get('name')
+            student_lastname = json.get('lastname')
+            student_semester = json.get('semester')
+            student_field_id = json.get('fieldId')
+
+            if not student_name:
+                return {"error": "student name is required"}
+            if not student_lastname:
+                return {"error": "student lastname is required"}
+            if not student_semester:
+                return {"error": "student semester is required"}
+            if not student_field_id:
+                return {"error": "student fieldId is required"}
+           
+            db.session.add(Student(name=student_name, lastname=student_lastname, semester=student_semester, field_id=student_field_id))
+            db.session.commit()
+        except Exception as e:
+            return {"error": e}
+        return {"status_code": 200, "description": f"successfully added a new student {student_name} {student_lastname}"}
+        
     @app.get('/api/v1/degrees')
     def degrees():
         try:
             degrees = db.session.query(Degree)
             result = DegreesHelper(degrees).jsonify()
+
             return result
         except Exception as e:
             return {"error": str(e)}
@@ -89,7 +123,7 @@ def create_app(test_config=None):
             return {"error": str(e)}
         
     @app.post("/api/v1/degrees")
-    def post_degrees():
+    def post_degree():
         try: 
             json = request.get_json()
             degree_name = json.get('name')
@@ -103,21 +137,32 @@ def create_app(test_config=None):
             return {"error": e}
         return {"status_code": 200, "description": f"successfully added a new degree {degree_name}"}
     
-    @app.put("/api/v1/degrees/<int:id>")
-    def put_degrees(id):
-        try: 
-            json = request.get_json()
-            degree_name = json.get('name')
-            degree = db.session.query(Degree).filter(Degree.id == id)
-            degree_object = DegreeHelper().deserialize(degree)
-            degree_object.name = degree_name
-            degree = DegreeHelper().serialize(degree_object)
+    # @app.put("/api/v1/degrees/<int:id>")
+    # def put_degrees(id):
+    #     try: 
+    #         json = request.get_json()
+    #         degree_name = json.get('name')
+    #         degree = db.session.query(Degree).filter(Degree.id == id)
+    #         degree_object = DegreeHelper().deserialize(degree)
+    #         degree_object.name = degree_name
+    #         degree = DegreeHelper().serialize(degree_object)
 
+    #         db.session.commit()
+
+    #     except Exception as e:
+    #         return {"error": e}
+    #     return {"status_code": 200, "descritpion": f"successfully udpated degree of id {deg}"}
+
+    @app.delete("/api/v1/degrees/<int:id>")
+    def delete_degree(id):
+        try:
+            db.session.query(Degree).filter(Degree.id == id).delete()
+            # if(degree.id != id):
+            #     return {"status_code": "400", "description": "degree does not exist in db"}
             db.session.commit()
-
+            return {"status_code": 200, "description": f"successfully deleted degree" }
         except Exception as e:
-            return {"error": e}
-        return {"status_code": 200, "descritpion": f"successfully udpated degree of id {deg}"}
+            return {"error": str(e)}
 
     @app.get('/api/v1/fields')
     def fields():
@@ -138,6 +183,33 @@ def create_app(test_config=None):
             return result
         except Exception as e:
             return {"error": str(e)}
+
+    @app.delete("/api/v1/fields/<int:id>")
+    def delete_field(id):
+        try:
+            db.session.query(Field).filter(Field.id == id).delete()
+            db.session.commit()
+            return {"status_code": 200, "description": f"successfully deleted Field" }
+        except Exception as e:
+            return {"error": str(e)}
+        
+    @app.post("/api/v1/fields")
+    def post_field():
+        try: 
+            json = request.get_json()
+            field_name = json.get('name')
+            field_degree_id = json.get('degreeId')
+
+            if not field_name:
+                return {"error": "field name is required"}
+            if not field_degree_id:
+                return {"error": "field degreeId is required"}
+           
+            db.session.add(Field(name=field_name, degree_id=field_degree_id))
+            db.session.commit()
+        except Exception as e:
+            return {"error": e}
+        return {"status_code": 200, "description": f"successfully added a new field {field_name}"}
         
     @app.get('/api/v1/grades')
     def grades():
@@ -159,6 +231,36 @@ def create_app(test_config=None):
         except Exception as e:
             return {"error": str(e)}
         
+    @app.delete("/api/v1/grades/<int:id>")
+    def delete_grade(id):
+        try:
+            db.session.query(Grade).filter(Grade.id == id).delete()
+            db.session.commit()
+            return {"status_code": 200, "description": f"successfully deleted Grade" }
+        except Exception as e:
+            return {"error": str(e)}       
+        
+    @app.post("/api/v1/grades")
+    def post_grade():
+        try: 
+            json = request.get_json()
+            grade_grade = json.get('grade')
+            grade_student_id = json.get('studentId')
+            grade_subject_id = json.get('subjectId')
+
+            if not grade_grade:
+                return {"error": "grade is required"}
+            if not grade_student_id:
+                return {"error": "grade studentId is required"}
+            if not grade_subject_id:
+                return {"error": "grade subjectId is required"}
+           
+            db.session.add(Grade(grade=grade_grade, student_id=grade_student_id, subject_id=grade_subject_id))
+            db.session.commit()
+        except Exception as e:
+            return {"error": e}
+        return {"status_code": 200, "description": f"successfully added a new grade {grade_grade}"}
+
     @app.get('/api/v1/subjects')
     def subjects():
         try:
@@ -178,5 +280,32 @@ def create_app(test_config=None):
             return result
         except Exception as e:
             return {"error": str(e)}
+        
+    @app.delete("/api/v1/subjects/<int:id>")
+    def delete_subject(id):
+        try:
+            db.session.query(Subject).filter(Subject.id == id).delete()
+            db.session.commit()
+            return {"status_code": 200, "description": f"successfully deleted Subject" }
+        except Exception as e:
+            return {"error": str(e)}  
+        
+    @app.post("/api/v1/subjects")
+    def post_subject():
+        try: 
+            json = request.get_json()
+            subject_name = json.get('name')
+            subject_field_id = json.get('fieldId')
+
+            if not subject_name:
+                return {"error": "subject name is required"}
+            if not subject_field_id:
+                return {"error": "subject fieldId is required"}
+           
+            db.session.add(Subject(name=subject_name, field_id=subject_field_id))
+            db.session.commit()
+        except Exception as e:
+            return {"error": e}
+        return {"status_code": 200, "description": f"successfully added a new Subject {subject_name}"}
 
     return app
